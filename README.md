962898997781d29d98512cd9cb3887eb882f3f22
Ben Lantaff <ben@benlantaff.com>
2024-06-09 16:34:03 -0400

    made the app self update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # bolo-royal
-Bolo-Royal
+
+Bolo-Royal w/ self updates
+
+## Updater & Publishing
+
+- Make sure you change `"version": "1.0.2"`.
+- Draft a release with the version
+- `npm run release-windows`
--- preload.js
+++ preload.js
@@ -1,10 +1,19 @@
+const pkg = require("./package.json");
+const os = require("node:os");
+const { ipcRenderer } = require("electron");
+
 window.addEventListener("DOMContentLoaded", () => {
+  const exitButton = document.getElementById("exit");
+
+  exitButton.addEventListener("click", (e) => {
+    ipcRenderer.send("exit");
+  });
+
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector);
     if (element) element.innerText = text;
   };
 
-  for (const type of ["chrome", "node", "electron"]) {
-    replaceText(`${type}-version`, process.versions[type]);
-  }
+  replaceText(`app-version`, `v${pkg.version}`);
+  replaceText(`platform`, os.platform);
 });
